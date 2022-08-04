8f9d0b4fdb7797bdcf1491522416d28c53d436eb
Rizki Putra Wicaksono <rizkiputra1100@gmail.com>
2022-08-05 01:02:52 +0700

    end

--- src/screens/Spesific.js
+++ src/screens/Spesific.js
@@ -61,7 +61,7 @@ const Spesific = () => {
         <Card
           sx={{
             display: "flex",
-            padding: 10,
+
             boxShadow: "rgba(0, 0, 0, 0.25) 0px 25px 50px -12px",
           }}
         >
